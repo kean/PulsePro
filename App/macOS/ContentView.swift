3e5e8ae40310f6291137c5937454f22da2f4c151
kean <grebenyuk.alexander@gmail.com>
2020-02-16 16:06:18 -0500

    Add macOS target

---
+++ App/macOS/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  macOS
+//
+//  Created by Alexander Grebenyuk on 16.02.2020.
+//  Copyright © 2020 kean. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
