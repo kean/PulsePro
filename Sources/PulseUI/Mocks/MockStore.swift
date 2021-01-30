87122481f001efa4d578f6fd92bfda90b379dbab
kean <grebenyuk.alexander@gmail.com>
2021-01-30 16:48:39 -0500

    MockStore create using swift-log APIs instead of direct CoreData access

--- Sources/PulseUI/Mocks/MockStore.swift
+++ Sources/PulseUI/Mocks/MockStore.swift
@@ -51,68 +51,31 @@ private extension NSManagedObject {
 private func populateStore(_ store: LoggerMessageStore) {
     precondition(Thread.isMainThread)
 
-    let moc = store.container.viewContext
-
-    func addMessage(_ closure: (MessageEntity) -> Void) {
-        let message = MessageEntity(using: moc)
-        closure(message)
-        moc.insert(message)
+    func logger(named: String) -> Logger {
+        var logger = Logger(label: named, factory: { PersistentLogHandler(label: $0, store: store) })
+        logger.logLevel = .trace
+        return logger
     }
 
-    addMessage {
-        $0.createdAt = Date() - 0.11
-        $0.level = Logger.Level.info.rawValue
-        $0.label = "application"
-        $0.session = PersistentLogHandler.logSessionId.uuidString
-        $0.text = "UIApplication.didFinishLaunching"
-    }
+    logger(named: "application")
+        .log(level: .info, "UIApplication.didFinishLaunching")
 
-    addMessage {
-        $0.createdAt = Date() - 0.1
-        $0.level = Logger.Level.info.rawValue
-        $0.label = "application"
-        $0.session = PersistentLogHandler.logSessionId.uuidString
-        $0.text = "UIApplication.willEnterForeground"
-    }
+    logger(named: "application")
+        .log(level: .info, "UIApplication.willEnterForeground")
 
-    addMessage {
-        $0.createdAt = Date() - 0.095
-        $0.level = Logger.Level.trace.rawValue
-        $0.label = "auth"
-        $0.session = PersistentLogHandler.logSessionId.uuidString
-        $0.text = "Instantiated Session"
-    }
+    logger(named: "auth")
+        .log(level: .trace, "Instantiated Session")
 
-    addMessage {
-        $0.createdAt = Date() - 0.092
-        $0.level = Logger.Level.trace.rawValue
-        $0.label = "auth"
-        $0.session = PersistentLogHandler.logSessionId.uuidString
-        $0.text = "Instantiated the new login request"
-    }
+    logger(named: "auth")
+        .log(level: .trace, "Instantiated the new login request")
 
-    addMessage {
-        $0.createdAt = Date() - 0.07
-        $0.level = Logger.Level.debug.rawValue
-        $0.label = "auth"
-        $0.session = PersistentLogHandler.logSessionId.uuidString
-        $0.text = "🌐 Will authorize user with name \"kean@github.com\""
-    }
+    logger(named: "auth")
+        .log(level: .debug, "🌐 Will authorize user with name \"kean@github.com\"")
 
-    addMessage {
-        $0.createdAt = Date() - 0.05
-        $0.level = Logger.Level.warning.rawValue
-        $0.label = "auth"
-        $0.session = PersistentLogHandler.logSessionId.uuidString
-        $0.text = "🌐 Authorization request failed with error 500"
-    }
+    logger(named: "auth")
+        .log(level: .warning, "🌐 Authorization request failed with error 500")
 
-    addMessage {
-        $0.createdAt = Date() - 0.04
-        $0.level = Logger.Level.debug.rawValue
-        $0.label = "auth"
-        $0.session = PersistentLogHandler.logSessionId.uuidString
-        $0.text = """
+    let stackTrace = """
         Replace this implementation with code to handle the error appropriately. fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
 
         2015-12-08 15:04:03.888 Conversion[76776:4410388] call stack:
@@ -132,19 +95,13 @@ private func populateStore(_ store: LoggerMessageStore) {
             20  libdyld.dylib                       0x34f34873 <redacted> + 2
         )
         """
-    }
 
-    addMessage {
-        $0.createdAt = Date() - 0.03
-        $0.level = Logger.Level.critical.rawValue
-        $0.label = "default"
-        $0.session = PersistentLogHandler.logSessionId.uuidString
-        $0.text = "💥 0xDEADBEEF"
-    }
+    logger(named: "auth")
+        .log(level: .debug, .init(stringLiteral: stackTrace))
 
-//    DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(2)) {
-//        populateStore(logger)
-//    }
+    logger(named: "default")
+        .log(level: .critical, "💥 0xDEADBEEF")
 
-    try! moc.save()
+    // Wait until everything is stored
+    store.container.viewContext.performAndWait {}
 }
