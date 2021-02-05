fd0ff7f737870545e1a82f6c081004719bcec638
kean <grebenyuk.alexander@gmail.com>
2021-02-04 19:46:20 -0500

    Simplify taskWithId

--- Sources/PulseUI/Extensions/Pulse+PulseUI.swift
+++ Sources/PulseUI/Extensions/Pulse+PulseUI.swift
@@ -8,27 +8,15 @@ import Logging
 #if DEBUG
 extension LoggerMessageStore {
     func taskIdWithURL(_ url: URL) -> String? {
-        let messages = ((try? allMessages()) ?? []).filter {
-            $0.metadata.contains {
-                $0.key == NetworkLoggerMetadataKey.eventType.rawValue && $0.value == NetworkLoggerEventType.taskDidComplete.rawValue
-            }
-        }
-
-        func event(for message: MessageEntity) -> NetworkLoggerEvent.TaskDidComplete? {
-            guard let payload = message.metadata.first(where: { $0.key == NetworkLoggerMetadataKey.payload.rawValue })?.value else {
-                return nil
-            }
-            guard let responseBody = payload.data(using: .utf8) else {
-                return nil
-            }
-            return try? JSONDecoder().decode(NetworkLoggerEvent.TaskDidComplete.self, from: responseBody)
-        }
-
-        let message = messages.first(where: {
-            event(for: $0)?.request.url == url
-        })
-
-        return message?.metadata.first(where: { $0.key == NetworkLoggerMetadataKey.taskId.rawValue })?.value
+        return (try? allMessages())?
+            .lazy
+            .compactMap(NetworkLoggerMessage.init)
+            .first(where: {
+                guard case let .taskDidComplete(event) = $0.event else {
+                    return false
+                }
+                return event.request.url == url
+            })?.taskId
     }
 }
 #endif
