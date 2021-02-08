6c8dbe8e9e4c0d12a4199ef956736ffd14389e07
kean <grebenyuk.alexander@gmail.com>
2021-02-08 18:37:19 -0500

    Try another workaround with multiple targes

--- Package.swift
+++ Package.swift
@@ -12,17 +12,24 @@ let package = Package(
         .watchOS(.v6)
     ],
     products: [
-        .library(name: "PulseUI", targets: ["PulseUI", "BinaryDependencyWorkaround"]),
+        .library(name: "PulseUI", targets: ["PulseUITarget"]),
     ],
     dependencies: [
         .package(url: "https://github.com/kean/Pulse.git", from: "0.6.0")
     ],
     targets: [
+        .target(
+            name: "PulseUITarget",
+            dependencies: [.target(name: "PulseUIWrapper")]
+        ),
+        .target(
+            name: "PulseUIWrapper",
+            dependencies: [.target(name: "PulseUI"), "Pulse"]
+        ),
         .binaryTarget(
             name: "PulseUI",
             url: "https://github.com/kean/Pulse/files/5947467/PulseUI-0.9.0.zip",
             checksum: "4696e9f408cfb6a684f604c7507bca4c7018de3eb15f1034f8e442d9ecd6c61a"
-        ),
-        .target(name: "BinaryDependencyWorkaround", dependencies: ["PulseUI", "Pulse"])
+        )
     ]
 )
