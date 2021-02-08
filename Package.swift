a94dbfd88ddc1f742773566bc60459a56d8cb0b9
kean <grebenyuk.alexander@gmail.com>
2021-02-08 18:59:17 -0500

    Simplify setup

--- Package.swift
+++ Package.swift
@@ -9,18 +9,9 @@ let package = Package(
         .iOS(.v13)
     ],
     products: [
-        .library(name: "PulseUI", targets: ["PulseUIWrapper"]),
-    ],
-    dependencies: [
-        .package(url: "https://github.com/kean/Pulse.git", from: "0.6.0")
+        .library(name: "PulseUI", targets: ["PulseUI"]),
     ],
     targets: [
-        // Fake target to make dependencies work
-        // For more info see https://forums.swift.org/t/swiftpm-binary-target-with-sub-dependencies/40197/6
-        .target(
-            name: "PulseUIWrapper",
-            dependencies: [.target(name: "PulseUI"), "Pulse"]
-        ),
         .binaryTarget(
             name: "PulseUI",
             url: "https://github.com/kean/Pulse/files/5947467/PulseUI-0.9.0.zip",
