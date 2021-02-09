6c4eac21ddb0d1de6a34502e492d3a324d4738ec
kean <grebenyuk.alexander@gmail.com>
2021-02-08 19:17:51 -0500

    Dynamic linkage

--- Package.swift
+++ Package.swift
@@ -9,9 +9,18 @@ let package = Package(
         .iOS(.v13)
     ],
     products: [
-        .library(name: "PulseUI", targets: ["PulseUI"]),
+        .library(name: "PulseUI", targets: ["PulseUIWrapper"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/kean/Pulse.git", .branch("dyn"))
     ],
     targets: [
+        // Fake target to make dependencies work
+        // For more info see https://forums.swift.org/t/swiftpm-binary-target-with-sub-dependencies/40197/6
+        .target(
+            name: "PulseUIWrapper",
+            dependencies: [.target(name: "PulseUI"), "Pulse"]
+        ),
         .binaryTarget(
             name: "PulseUI",
             url: "https://github.com/kean/Pulse/files/5947467/PulseUI-0.9.0.zip",
