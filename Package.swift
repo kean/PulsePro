75bb471f5eced71d3ab02eec1b5a93ea33c61a5f
kean <grebenyuk.alexander@gmail.com>
2021-02-08 22:05:31 -0500

    Switch to use PulseCore dynamic framework

--- Package.swift
+++ Package.swift
@@ -23,8 +23,8 @@ let package = Package(
         ),
         .binaryTarget(
             name: "PulseUI",
-            url: "https://github.com/kean/Pulse/files/5947467/PulseUI-0.9.0.zip",
-            checksum: "4696e9f408cfb6a684f604c7507bca4c7018de3eb15f1034f8e442d9ecd6c61a"
+            url: "https://github.com/kean/Pulse/files/5948457/PulseUI-0.9.0.zip",
+            checksum: "3a50239dfbc620638f81fd7dba32dab3f05e823d65f7c2e3547b72f0a0c11709"
         )
     ]
 )
