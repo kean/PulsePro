18fba7d01bcb1c5c4c37f7b4ba4356b52f75bbb1
Alexander Grebenyuk <grebenyuk.alexander@gmail.com>
2021-03-27 21:56:32 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -25,13 +25,13 @@ let package = Package(
         ),
         .binaryTarget(
             name: "PulseCore",
-            url: "https://github.com/kean/Pulse/files/6177926/PulseCore-0.11.0.zip",
-            checksum: "7eb094476eba2e9c0e7c043862b21e239bca4858b27f5d1d6906019393958c99"
+            url: "https://github.com/kean/Pulse/files/6216722/PulseCore-0.12.0.zip",
+            checksum: "06a32bec39fc9cda9905f636ef1fe49696c3acee29e0a35c130a3bf6f79ba395"
         ),
         .binaryTarget(
             name: "PulseUI",
-            url: "https://github.com/kean/Pulse/files/6177929/PulseUI-0.11.0.zip",
-            checksum: "07fd043106755f55a17bc24158c4ad6764adbcde00e64e13fea30b3318d421a9"
+            url: "https://github.com/kean/Pulse/files/6216724/PulseUI-0.12.0.zip",
+            checksum: "f02c8a9f6c5c21a3eccdbdc436f6983a1d33449b17e29da37f16ba589eff114d"
         )
     ]
 )
